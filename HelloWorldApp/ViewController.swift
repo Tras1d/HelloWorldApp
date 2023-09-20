1f64fa79ac629d899cf39f429300f239ba95aa2c
Dmitriy <trasid46@mail.ru>
2023-09-20 21:26:23 +0300

    Test

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    let screen = UIScreen.main.bounds
+//    let square = UIView()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        let frame = CGRect(x: screen.width/2 - 50, y: screen.height/2 - 50, width: 100 , height: 100)
+//        square.frame = frame
+//        square.backgroundColor = .blue
+//        view.addSubview(square)
     }
+    
+    
 
 
 }
